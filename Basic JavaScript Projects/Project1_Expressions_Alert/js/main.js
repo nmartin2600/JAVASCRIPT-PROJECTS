d62461d97b822a40f0a6b6a5054e91df2487cf77
Nicholas Martin <57820367+nmartin2600@users.noreply.github.com>
2026-05-28 16:42:32 -0400

    Complete Project 1 Expressions Alert

---
+++ Basic JavaScript Projects/Project1_Expressions_Alert/js/main.js
@@ -0,0 +1,7 @@
+// Create two variables with string values
+const sent1 = 'This is the beginning of the string';
+const sent2 = ' and this is the end of the string';
+// Display window that will show concatenation of the two strings on page load
+window.alert(sent1 + sent2);
+// Write the result of this addition to the page body
+document.write(2 + 2);
