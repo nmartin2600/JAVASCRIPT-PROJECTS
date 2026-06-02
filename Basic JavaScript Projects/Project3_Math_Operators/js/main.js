8b9e1c89ed747cf2582d7e3b450ec30769272e5f
Nicholas Martin <57820367+nmartin2600@users.noreply.github.com>
2026-06-02 13:59:29 -0400

    Add p element and event handlers for each operation

--- Basic JavaScript Projects/Project3_Math_Operators/js/main.js
+++ Basic JavaScript Projects/Project3_Math_Operators/js/main.js
@@ -1,34 +1,34 @@
 /*Get p element with id "math" and replace text with sum*/
 function addToParagraph() {
-    let para = document.getElementById("math");
+    let para = document.getElementById("add");
     const sum = 5 + 5;
     para.innerHTML = "5 + 5 = " + sum;
 }
 
 /*Get p element with id "math" and replace text with remainder*/
 function subtractFromParagraph() {
-    let para = document.getElementById("math");
+    let para = document.getElementById("subtract");
     const remainder = 10 - 7;
     para.innerHTML = "10 - 7 = " + remainder;
 }
 
 /*Get p element with id "math" and replace text with product*/
 function multiplyParagraph() {
-    let para = document.getElementById("math");
+    let para = document.getElementById("multiply");
     const product = 2 * 2;
     para.innerHTML = "2 * 2 = " + product;
 }
 
 /*Get p element with id "math" and replace text with quotient*/
 function divideParagraph() {
-    let para = document.getElementById("math");
+    let para = document.getElementById("division");
     const quotient = 20 / 5;
     para.innerHTML = "20 / 5 = " + quotient;
 }
 
 /*Get p element with id "math" and replace text with result*/
 function multiParagraph() {
-    let para = document.getElementById("math");
+    let para = document.getElementById("multiple");
     const result = (2 + 2) * (10 - 4) / 6;
     para.innerHTML = "The result of adding 2 plus 2, multiplying it by 10 minus 4, "
         + "then dividing by 6 is: " + result;
@@ -36,28 +36,28 @@ function multiParagraph() {
 
 /*Get p element with id "math" and replace text with remainder*/
 function modulusParagraph() {
-    let para = document.getElementById("math");
+    let para = document.getElementById("modulus");
     const remainder = 20 % 3;
     para.innerHTML = "The remainder of dividing 20 by 3 is: " + remainder;
 }
 
 /*Get p element with id "math" and replace text with negation*/
 function negateParagraph() {
-    let para = document.getElementById("math");
+    let para = document.getElementById("negate");
     const operand = 10;
     para.innerHTML = "The negation of 10 is: " + (-operand);
 }
 
 /*Get p element with id "math" and replace text with random number*/
 function randomParagraph() {
-    let para = document.getElementById("math");
+    let para = document.getElementById("random");
     const num = Math.random() * 11;
     para.innerHTML = "Random number between 0 and 11: " + num;
 }
 
 /*Get p element with id "math" and replace text with rounded number*/
 function roundParagraph() {
-    let para = document.getElementById("math");
+    let para = document.getElementById("round");
     const num = Math.round(4.6);
     para.innerHTML = "4.6 rounded up is: " + num;
 }
