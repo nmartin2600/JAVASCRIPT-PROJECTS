c1ae91e15c50ba66bd47d6601ecfeef5f7a790f0
Nicholas Martin <57820367+nmartin2600@users.noreply.github.com>
2026-06-22 16:58:25 -0400

    Complete Switch Statement Assignment

---
+++ Advanced JavaScript Projects/JavaScript/JavaScript_1.js
@@ -0,0 +1,30 @@
+function color_choice() {
+    let color_output;
+    let colors = document.getElementById("color_input").value;
+    let color_string = " is a great color!";
+
+    switch(colors) {
+        case "Red":
+            color_output = "Red" + color_string;
+            break;
+        case "Yellow":
+            color_output = "Yellow" + color_string;
+            break;
+        case "Green":
+            color_output = "Green" + color_string;
+            break;
+        case "Blue":
+            color_output = "Blue" + color_string;
+            break;
+        case "Pink":
+            color_output = "Pink" + color_string;
+            break;
+        case "Purple":
+            color_output = "Purple" + color_string;
+            break;
+        default:
+            color_output = "Please enter a color exactly as written on the above list.";
+    }
+
+    document.getElementById("output").innerHTML = color_output;
+}
