a06f267c9bd550acd94357b88b55865a434b7ad7
Nicholas Martin <57820367+nmartin2600@users.noreply.github.com>
2026-07-15 17:14:53 -0400

    Add JS functionality for adding vegetables to order

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -1,3 +1,4 @@
+// This function initializes the receipt based on the selected pizza size
 function getReceipt() {
     // This initializes our string so it can get passed from
     // function to function, growing line by line into a full receipt
@@ -6,6 +7,7 @@ function getReceipt() {
     var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
 
+    // Loop through pizza size input array to see which one was selected
     for (var i = 0; i < sizeArray.length; i++) {
         if (sizeArray[i].checked) {
             var selectedSize = sizeArray[i].value;
@@ -13,6 +15,7 @@ function getReceipt() {
         }
     }
 
+    // Set price of pizza size based on above selection
     if (selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     }
@@ -40,15 +43,17 @@ function getReceipt() {
     console.log(selectedSize + " = $" + sizeTotal + ".00");
     console.log("Size text1: " + text1);
     console.log("Subtotal: $" + runningTotal + ".00");
-    // These variables will get passed on to each function
+    // These variables will get passed on to each function, starting with getTopping()
     getTopping(runningTotal, text1);
 }
 
+// This function adds any toppings selected to the receipt started from getReceipt()
 function getTopping(runningTotal, text1) {
     var toppingTotal = 0;
     var selectedTopping = [];
     var toppingArray = document.getElementsByClassName("toppings");
 
+    // Loop through topping input array to see which ones were selected
     for (var j = 0; j < toppingArray.length; j++) {
         if (toppingArray[j].checked) {
             selectedTopping.push(toppingArray[j].value);
@@ -59,9 +64,11 @@ function getTopping(runningTotal, text1) {
 
     var toppingCount = selectedTopping.length;
 
+    // If more than one topping selected, subtract $1 for 1 free topping
     if (toppingCount > 1) {
         toppingTotal = toppingCount - 1;
     }
+    // No toppings were selected
     else {
         toppingTotal = 0;
     }
@@ -70,6 +77,41 @@ function getTopping(runningTotal, text1) {
     console.log("Total selected topping items: " + toppingCount);
     console.log(toppingCount + " topping - 1 free topping = " + "$" + toppingTotal + ".00");
     console.log("Topping text1: " + text1);
+    // Pass variables to getVeggie()
+    getVeggie(runningTotal, text1);
+}
+
+// This function adds any vegetables selected to the receipt and displays the grand total of the order
+function getVeggie(runningTotal, text1) {
+    var vegTotal = 0;
+    var selectedVeg = [];
+    var vegArray = document.getElementsByClassName("vegetables");
+
+    // Loop through vegetable input array to see which ones were selected
+    for (var k = 0; k < vegArray.length; k++) {
+        if (vegArray[k].checked) {
+            selectedVeg.push(vegArray[k].value);
+            console.log("Selected vegetable item: (" + vegArray[k].value + ")");
+            text1 += vegArray[k].value + "<br>";
+        }
+    }
+
+    var vegCount = selectedVeg.length;
+
+    // If more than one vegetable selected, subtract $1 for 1 free vegetable
+    if (vegCount > 1) {
+        vegTotal = vegCount - 1;
+    }
+    // No vegetables were selected
+    else {
+        vegTotal = 0;
+    }
+
+    runningTotal += vegTotal;
+    console.log("Total selected vegetable items: " + vegCount);
+    console.log(vegCount + " vegetable - 1 free vegetable = " + "$" + vegTotal + ".00");
+    console.log("Vegetable text1: " + text1);
+    // Display selected items and order total on menu page
     console.log("Purchase Total: " + "$" + runningTotal + ".00");
     document.getElementById("showText").innerHTML = text1;
     document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"
